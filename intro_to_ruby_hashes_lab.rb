b574c762cb357438c6ed0a4dff337853d243f998
Liz Laffitte <liz.kossakowski@gmail.com>
2019-09-06 10:15:56 +0000

    Automatically backed up by Learn

--- intro_to_ruby_hashes_lab.rb
+++ intro_to_ruby_hashes_lab.rb
@@ -28,6 +28,8 @@ def monopoly_with_third_tier
         :pieces => 4,
         :rent_in_dollars => {
           :one_piece_owned => 25,
+          :two_pieces_owned => 50,
+          :three_pieces_owned => 150,
           :names => 1,
           :help => 1,
           :whatever =>1
