7714efb73711a66264ad436efcd5236f0b6575d6
Liz Laffitte <liz.kossakowski@gmail.com>
2019-09-06 10:12:32 +0000

    Automatically backed up by Learn

--- intro_to_ruby_hashes_lab.rb
+++ intro_to_ruby_hashes_lab.rb
@@ -26,7 +26,12 @@ def monopoly_with_third_tier
     :railroads => 
       {
         :pieces => 4,
-        :rent_in_dollars => {}
+        :rent_in_dollars => {
+          :amount => 1,
+          :month => 1,
+          :help => 1,
+          :whatever =>
+        }
       } 
   }
 end
